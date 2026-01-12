fd1a1146fae3bf4b94539aa93c3b7c4879e65da0
umesh <69199558+494494@users.noreply.github.com>
2026-01-12 18:46:31 +0530

    Add files via upload

---
+++ lab1_13Questions/o2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int c,f;
+    cout<<"Enter temp. in degrees :- ";
+    cin>>c;
+    f=9*c/5+32;
+    cout<<"degree Fahrenhei = "<<f;
+    return 0;
+}
